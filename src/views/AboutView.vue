3c863d169e7677a18381fc7bbb56b2f582a5a0f9
thb <thbwork@163.com>
2023-03-09 19:34:21 +0800

    组合式API结合Pinia

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+// 组合式API结合Pinia
+import { useCounterStore } from '@/stores/counter'
+const store = useCounterStore()
+</script>
 <template>
   <div class="about">
+    <strong>组合式API(setup)结合Pinia</strong>
     <h1>This is an about page</h1>
+    <button @click="store.increment()">Add</button>
+    <div>
+      {{ store.count }}
+    </div>
   </div>
 </template>
 
@@ -9,6 +19,8 @@
   .about {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
   }
 }
